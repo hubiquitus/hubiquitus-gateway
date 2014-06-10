185571d78d62e761753cb00ce8457399b88fbd58
Nadim <nadim.boustani@gmail.com>
2014-06-10 14:40:41 +0200

    Add client heart beat support

--- index.js
+++ index.js
@@ -19,15 +19,17 @@ events.setMaxListeners(0);
 var defaultPort = 8888;
 var defaultPath = '/hubiquitus';
 var loginTimeout = 30000;
-var heartbeatFreq = 5000;
+var defaultHeartbeatFreq = 5000;
+var defaultClientTimeout = 2 * defaultHeartbeatFreq;
 
 /**
  * Create a gateway
  * @param [session] {function} session actor
  * @param [login] {function} loginentication function
+ * @param [opts] {object} options (heartbeatFreq, clientTimeout)
  * @returns {Gateway}
  */
-exports.createGateway = function (session, login) {
+exports.createGateway = function (session, login, opts) {
   if (!_.isFunction(session)) {
     logger.trace('session actor not provided or invalid; default implementation is used instead');
     session = null;
@@ -36,16 +38,23 @@ exports.createGateway = function (session, login) {
     logger.trace('login procedure not provided or invalid; default implementation is used instead');
     login = null;
   }
-  return new Gateway(session, login);
+
+  opts = opts || {};
+
+  if (!_.isNumber(opts.heartbeatFreq)) opts.heartbeatFreq = defaultHeartbeatFreq;
+  if (!_.isNumber(opts.clientTimeout)) opts.clientTimeout = defaultClientTimeout;
+
+  return new Gateway(session, login, opts);
 };
 
 /**
  * Gateway constructor
  * @param [login] {function} auth function
  * @param [session] {function} session actor
+ * @param [opts] {object} options (heartbeatFreq, clientTimeout)
  * @constructor
  */
-function Gateway(session, login) {
+function Gateway(session, login, opts) {
   EventEmitter.call(this);
 
   var _this = this;
@@ -54,7 +63,8 @@ function Gateway(session, login) {
   session = session || basicSession;
 
   _this.socks = {};
-  _this.heartbeatFreq = heartbeatFreq;
+  _this.heartbeatFreq = opts.heartbeatFreq;
+  _this.clientTimeout = opts.clientTimeout;
 
   var sockOptions = {
     log: function (level, message) {
@@ -71,8 +81,14 @@ function Gateway(session, login) {
       logger.debug('authentication delay timeout !');
       logout(sock);
     }, loginTimeout);
+    sock.hb = Date.now();
 
     sock.on('data', function (data) {
+      if (data === 'hb') {
+        sock.hb = Date.now();
+        return;
+      }
+
       var msg = decode(data);
       if (msg && msg.type) {
         switch (msg.type) {
@@ -165,6 +181,7 @@ function Gateway(session, login) {
     if (sock.identity) {
       _this.emit('disconnected', sock.identity);
       hubiquitus.removeActor(sock.identity);
+      delete sock.identity;
     }
     sock.close();
   }
@@ -179,6 +196,20 @@ function Gateway(session, login) {
     setTimeout(sendHeartbeat, _this.heartbeatFreq);
   }
   sendHeartbeat();
+
+  /**
+   * Check that all clients respond to the heartbeat in time
+   */
+  function checkClientsHeartbeat() {
+    var now = Date.now();
+    _.forOwn(_this.socks, function (sock) {
+      if (sock.hb + _this.clientTimeout < now) {
+        logout(sock);
+      }
+    });
+    setTimeout(checkClientsHeartbeat, 1000)
+  }
+  checkClientsHeartbeat();
 }
 
 util.inherits(Gateway, EventEmitter);
